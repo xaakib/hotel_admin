cf0c0889e5f6561ac337030d07347017107b6705
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-09-11 15:46:40 +0600

    working on other desing

---
+++ lib/components/TextFieldContainerDEcoration.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TextFieldContainerDEcoration extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainerDEcoration({Key? key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.06,
+      padding: EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              offset: Offset(0, 2),
+              blurRadius: 6,
+            ),
+          ]),
+      child: child,
+    );
+  }
+}
+
+  //  TextFieldContainerDEcoration(
+  //                       child: TextFormField(
+  //                         textAlign: TextAlign.start,
+  //                         controller: usernameCotnroller,
+  //                         validator: (value) {
+  //                           if (value!.isEmpty || value == null) {
+  //                             return "input you requerd";
+  //                           }
+  //                         },
+  //                         decoration: InputDecoration(
+  //                             hintText: "Mobile", border: InputBorder.none),
+  //                       ),
+  //                     ),
+  //                     SizedBox(height: 10),
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hotel_admin/components/textfield_container_decorations.dart';
+import 'package:hotel_admin/components/TextFieldContainerDEcoration.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -11,12 +11,32 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController emailController = TextEditingController();
+  final TextEditingController usernameCotnroller = TextEditingController();
+
   final TextEditingController passowrdController = TextEditingController();
   final TextEditingController confrimPassController = TextEditingController();
-  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController hotelnameCotnroller = TextEditingController();
+  final TextEditingController hotelOwnerNameCotnroller =
+      TextEditingController();
+  final TextEditingController emailCotnroller = TextEditingController();
+  final TextEditingController mobileNumberCotnroller = TextEditingController();
+  final TextEditingController telephoneNumberCotnroller =
+      TextEditingController();
+  final TextEditingController faxnumberCotnroller = TextEditingController();
+  final TextEditingController roadHouseNumberCotnroller =
+      TextEditingController();
+  final TextEditingController zipCodeCotnroller = TextEditingController();
+  final TextEditingController writeCopanyAddressCotnroller =
+      TextEditingController();
+  final TextEditingController aboutCotnroller = TextEditingController();
   final TextEditingController nameControler = TextEditingController();
-
+  final TextEditingController floorCotnroller = TextEditingController();
+  final TextEditingController buinessORTradeLicenceController =
+      TextEditingController();
+  final TextEditingController tINNumberCotnroller = TextEditingController();
+  final TextEditingController websiteUrlCotnroller = TextEditingController();
+  final TextEditingController aboutYourBuissinessController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,67 +52,60 @@ class _RegisterScreenState extends State<RegisterScreen> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Colors.grey.withOpacity(0.1),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Welcome TO",
-                    style: GoogleFonts.pacifico(
-                        color: Color(0xff0c328a),
-                        letterSpacing: 1,
-                        wordSpacing: 1,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 12),
-                  ),
-                  Text(
-                    "Hotel Admin",
-                    style: GoogleFonts.k2d(
-                        color: Color(0xff0c328a),
-                        letterSpacing: 1,
-                        wordSpacing: 1,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Form(
-                key: _formKey,
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 20),
-                    TextFeildContainerDecoration(
-                        emailController: usernameController),
-                    SizedBox(height: 10),
-                    TextFeildContainerDecoration(
-                        emailController: passowrdController),
-                    SizedBox(height: 10),
-                    TextFeildContainerDecoration(
-                        emailController: nameControler),
-                    SizedBox(height: 10),
-                    // ignore: deprecated_member_use
-                    RaisedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          print("DOne");
-                        } else {
-                          print("Not Done");
-                        }
-                      },
-                      child: Text("Set"),
-                    )
+                    Text(
+                      "Welcome TO",
+                      style: GoogleFonts.pacifico(
+                          color: Color(0xff0c328a),
+                          letterSpacing: 1,
+                          wordSpacing: 1,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12),
+                    ),
+                    Text(
+                      "Hotel Admin",
+                      style: GoogleFonts.k2d(
+                          color: Color(0xff0c328a),
+                          letterSpacing: 1,
+                          wordSpacing: 1,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24),
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      // ignore: deprecated_member_use
+                      RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            print("DOne");
+                          } else {
+                            print("Not Done");
+                          }
+                        },
+                        child: Text("Set"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
